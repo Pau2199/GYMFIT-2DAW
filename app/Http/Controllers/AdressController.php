bb70a7f9fe50360d37c5b422ed1ffb646b02f18f
Alex <alejandrosal1396@gmail.com>
2020-02-19 15:18:44 +0100

    Vista Carrito recuperada

--- app/Http/Controllers/AdressController.php
+++ app/Http/Controllers/AdressController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class AdressController extends Controller
 {
     
-    public function crearDireccion(Request $request){
+    public function crearDireccion($array){
         
         $adress = new Adress;
         $adress->type_way = $request
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 
 Route::get('/', function(){
-    return view('index');
+    return view('vistaCarrito');
 });
 
 Route::get('/formAgregarProducto', 'ProductController@create');
